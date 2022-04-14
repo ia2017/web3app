2271f82c67a5618aecb4042d2e0b27ed796f8b07
Imran bin Ahmad Azhar <imranahmazh@gmail.com>
2022-04-14 05:27:51 +0100

    welcome page done

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -24,10 +24,14 @@ const Welcome = () => {
 
 	}
 
+	const handleSubmit = () =>{
+
+	}
+
 	return (
 		<div className="flex w-full justify-center items-center">
-			<div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-				<div className=" flex flex-1 justify-start flex-col md:mr-10">
+			<div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+				<div className=" flex flex-1 justify-start flex-col mf:mr-10">
 					<h1 className="text-3x1 sm:text-5x1 text-white text-gradient py-1">Send Crypto <br /> across the world</h1>
 					<p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base"> Explore the crypto world. Buy and sell crypto.</p>
 					<button 
@@ -52,7 +56,7 @@ const Welcome = () => {
 						</div>
 				</div>
 				{/* md - medium devices , sm - small devices (mobile)*/}
-				<div className='Flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10'>
+				<div className='Flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10'>
 					<div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
 						<div className="flex justify-between flex-col w-full h-full">
 							<div className="flex justify-between items-start">
@@ -74,6 +78,23 @@ const Welcome = () => {
 					{/* Form */}
 					<div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
 						<Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}}/>
+						<Input placeholder="Amout (ETH)" name="amount" type="number" handleChange={() => {}}/>
+						<Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => {}}/>
+						<Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}}/>
+					{/* Need to fix length for loader */}
+						<div className="h-[1px] w-full bg-gray-400 my-2">
+							{false ? (
+								<Loader />
+							) : (
+								<button
+									type="button"
+									onClick={handleSubmit}
+									className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
+								>
+									Send Now
+								</button>
+							)}
+						</div>
 					</div>
 				</div>
 			</div>
